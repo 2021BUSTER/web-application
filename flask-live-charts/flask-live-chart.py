35f23fc126fcc6aa2b3934b758f22d3d4d02463d
shinyeji28 <71514775+shinyeji28@users.noreply.github.com>
2021-06-17 15:01:31 +0900

    헤더 수정

--- flask-live-charts/flask-live-chart.py
+++ flask-live-charts/flask-live-chart.py
@@ -1,7 +1,9 @@
 import json
+import sqlite3 
+import sys
 from time import time
 from random import random
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, redirect, url_for
 
 app = Flask(__name__)
 
@@ -16,8 +18,9 @@ def live_data():
     db = sqlite3.connect("DB.db")
     db.row_factory = sqlite3.Row
     query = db.execute("SELECT * FROM eyes").fetchall()
+    for q in 
     db.close()
-    data = [time() * 1000, query]
+    data = [time() * 1000, random()]
     #data = [time() * 1000, random() * 100]
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
