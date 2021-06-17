b7ea747bb57e5fd3fb4f4848c3a444d5656bdb88
shinyeji28 <71514775+shinyeji28@users.noreply.github.com>
2021-06-17 14:48:37 +0900

    not my own code
    go to tdiethe's github

---
+++ flask-live-charts/flask-live-chart.py
@@ -0,0 +1,27 @@
+import json
+from time import time
+from random import random
+from flask import Flask, render_template, make_response
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html', data='test')
+
+@app.route('/live-data')
+def live_data():
+    # Create a PHP array and echo it as JSON
+    db = sqlite3.connect("DB.db")
+    db.row_factory = sqlite3.Row
+    query = db.execute("SELECT * FROM eyes").fetchall()
+    db.close()
+    data = [time() * 1000, query]
+    #data = [time() * 1000, random() * 100]
+    response = make_response(json.dumps(data))
+    response.content_type = 'application/json'
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
