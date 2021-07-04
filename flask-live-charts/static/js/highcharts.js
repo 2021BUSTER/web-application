c911c4c81ae2e182ebd90d8513f5b48268441826
shinyeji28 <71514775+shinyeji28@users.noreply.github.com>
2021-07-04 22:22:39 +0900

    for pupil pose web code
    success!!!

--- flask-live-charts/static/js/highcharts.js
+++ flask-live-charts/static/js/highcharts.js
@@ -1,4 +1,5 @@
 var chart;
+var chart2;
 
 /**
  * Request data from the server, add it to the graph and set a timeout
@@ -11,9 +12,18 @@ function requestData() {
             var series = chart.series[0],
                 shift = series.data.length > 20; // shift if the series is
                                                  // longer than 20
-
+            var series2 = chart2.series[0],
+                shift2 = series2.data.length > 20; 
+            
+            var data1=[]
+            var data2=[]
+            data1.push(point[0]);
+            data1.push(point[1]);
+            data2.push(point[2]);
+            data2.push(point[3]);
             // add the point
-            chart.series[0].addPoint(point, true, shift);
+            chart.series[0].addPoint(data1, true, shift);
+            chart2.series[0].addPoint(data2, true, shift2);
 
             // call it again after one second
             setTimeout(requestData, 1000);
@@ -32,7 +42,7 @@ $(document).ready(function() {
             }
         },
         title: {
-            text: 'Live random data'
+            text: 'BUSTER PROJECT'
         },
         xAxis: {
             type: 'datetime',
@@ -48,7 +58,36 @@ $(document).ready(function() {
             }
         },
         series: [{
-            name: 'Random data',
+            name: 'date-time',
+            data: []
+        }]
+    });
+    chart2   = new Highcharts.Chart({
+        chart: {
+            renderTo: 'data-container1',
+            defaultSeriesType: 'spline',
+            events: {
+                load: requestData
+            }
+        },
+        title: {
+            text: 'BUSTER PROJECT'
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 150,
+            maxZoom: 20 * 1000
+        },
+        yAxis: {
+            minPadding: 0.2,
+            maxPadding: 0.2,
+            title: {
+                text: 'Value',
+                margin: 80
+            }
+        },
+        series: [{
+            name: 'date-time',
             data: []
         }]
     });
