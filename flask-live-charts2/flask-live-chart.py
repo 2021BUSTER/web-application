7d0c908b01476f8b74d8774670b7ef0a221e222a
shinyeji28 <71514775+shinyeji28@users.noreply.github.com>
2021-07-12 21:26:32 +0900

    Add files via upload

---
+++ flask-live-charts2/flask-live-chart.py
@@ -0,0 +1,45 @@
+import json
+import sqlite3 
+import sys
+from time import time
+from random import random
+from flask import Flask, render_template, make_response, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html', data='test')
+
+@app.route('/live-data',methods=["GET", "POST"])
+def live_data():    
+    db = sqlite3.connect("DB2.db")
+    #db.row_factory = sqlite3.Row
+    query = db.execute("SELECT value,datetime FROM concentration ORDER BY datetime DESC LIMIT 1").fetchall()
+    query_pose = db.execute("SELECT value,datetime FROM concentration ORDER BY datetime DESC LIMIT 1").fetchall()
+
+
+    for q in query:
+        concen_v=q[0]
+        concen_d=q[1]
+    
+    for q in query_pose:
+        pose_v=q[0]
+        pose_d=q[1] 
+    print("hi~~~~",concen_d,concen_v,pose_d,pose_v,"hi~~~")
+
+    data = [concen_d,concen_v,pose_d,pose_v]
+    #data = [time() * 1000,concen_v,time() * 1000,pose_v]
+   
+   # data = [time() * 1000, random() * 1000,time() * 100, random() * 100]
+    #data=[5,5,8,8]
+    response = make_response(json.dumps(data))
+    response.content_type = 'application/json'
+    
+    db.close()
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
---
+++ flask-live-charts2/static/js/highcharts.js
@@ -0,0 +1,106 @@
+var chart;
+var chart2;
+
+/**
+ * Request data from the server, add it to the graph and set a timeout
+ * to request again
+ */
+function requestData() {
+    $.ajax({
+        url: '/live-data',
+        success: function(point) {
+            var series = chart.series[0],
+                shift = series.data.length > 50; // shift if the series is
+                                                 // longer than 20
+            var series2 = chart2.series[0],
+                shift2 = series2.data.length > 50; 
+            
+            var data1=[]
+            var data2=[]
+            data1.push(point[0]);
+            data1.push(point[1]);
+            data2.push(point[2]);
+            data2.push(point[3]);
+            // add the point
+            chart.series[0].addPoint(data1, true, shift);
+            chart2.series[0].addPoint(data2, true, shift2);
+
+            // call it again after one second
+            setTimeout(requestData, 60000);
+        },
+        cache: false
+    });
+}
+
+Highcharts.setOptions({
+    global: {
+        useUTC: false
+    }
+});
+
+$(document).ready(function() {
+    chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'data-container',
+            defaultSeriesType: 'spline',
+            events: {
+                load: requestData
+            }
+        },
+        title: {
+            text: 'BUSTER PROJECT'
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 150,
+            maxZoom: 20 * 1000,
+            labels: {
+                formatter: function() {
+                  return Highcharts.dateFormat('%H:%M:%S', this.value);
+                }
+              }
+        },
+        yAxis: {
+            minPadding: 0.2,
+            maxPadding: 0.2,
+            title: {
+                text: 'Value',
+                margin: 80
+            }
+        },
+        series: [{
+            name: 'date-time',
+            data: []
+        }]
+    });
+
+    chart2   = new Highcharts.Chart({
+        chart: {
+            renderTo: 'data-container1',
+            defaultSeriesType: 'spline',
+            events: {
+                load: requestData
+            }
+        },
+        title: {
+            text: 'BUSTER PROJECT'
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 150,
+            maxZoom: 20 * 1000
+        },
+        yAxis: {
+            minPadding: 0.2,
+            maxPadding: 0.2,
+            title: {
+                text: 'Value',
+                margin: 80
+            }
+        },
+        series: [{
+            name: 'date-time',
+            data: []
+        }]
+    });
+});
